6051404c1855ad4dc37dd748bca8792196f895e0
Andrew Rozhnev <foo@bar.com>
2022-10-24 11:51:03 +0800

    feat(posts): Add paginator in posts app :sparkles:

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
@@ -6,10 +7,14 @@ from .models import Group, Post
 def index(request):
     template = 'posts/index.html'
 
-    last10_posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')
+    paginator = Paginator(posts, 5)
+
+    page_number = request.GET.get('page')
+    current_page_posts = paginator.get_page(page_number)
+
     context = {
-        'title': 'Latest updates',
-        'last10_posts': last10_posts,
+        'current_page_posts': current_page_posts,
     }
     return render(request, template, context)
 
@@ -18,9 +23,14 @@ def group_list(request, slug: str):
     template = 'posts/group_list.html'
 
     group = get_object_or_404(Group, slug=slug)
-    group_posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    group_posts = Post.objects.filter(group=group).order_by('-pub_date')
+    paginator = Paginator(group_posts, 5)
+
+    page_number = request.GET.get('page')
+    current_page_posts = paginator.get_page(page_number)
+
     context = {
         'group': group,
-        'group_posts': group_posts,
+        'current_page_posts': current_page_posts,
     }
     return render(request, template, context)
