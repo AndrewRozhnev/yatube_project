981c71f65e050c032dafac244243608a93d859ea
Andrew Rozhnev <andrewrozhnev@gmail.com>
2023-03-14 21:21:04 +0800

    refactor: Rename form.py to forms.py for Django consistency

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
 
-from .form import SignUpForm
+from .forms import SignUpForm
 
 
 class SignUpView(CreateView):
